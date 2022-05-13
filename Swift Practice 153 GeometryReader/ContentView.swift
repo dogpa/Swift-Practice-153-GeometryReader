8a78cb4fd6f4c3e0b221f947564f0576fa7a08c8
Dogpa <dogpa0130@gmail.com>
2022-05-13 23:29:06 +0800

    Initial Commit

---
+++ Swift Practice 153 GeometryReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 153 GeometryReader
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 153 GeometryReader/Swift_Practice_153_GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_153_GeometryReaderApp.swift
+//  Swift Practice 153 GeometryReader
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_153_GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
